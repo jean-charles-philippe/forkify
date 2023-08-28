1630bc892bb7327f232321b4f4d9864fae5d460d
Jean Charles <jean-charles.philippe@outlook.com>
2023-08-27 22:24:42 -0200

    mvc

---
+++ src/js/controller.js
@@ -0,0 +1,41 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime';
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(
+                new Error(`Request took too long! Timeout after ${s} second`)
+            );
+        }, s * 1000);
+    });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+    try {
+        const id = window.location.hash.slice(1);
+        if (!id) return;
+        recipeView.renderSpinner();
+        await model.loadRecipe(id);
+        recipeView.render(model.state.recipe);
+    } catch (error) {
+        alert(error);
+    }
+};
+
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev =>
+    window.addEventListener(ev, controlRecipes)
+);
+
+// if (module.hot) {
+//     module.hot.accept();
+// }
